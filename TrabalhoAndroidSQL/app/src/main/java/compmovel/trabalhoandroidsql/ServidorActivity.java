0b721e9a4681b3f61ba1963a406a1d8cee7680d0
Vitor Gonçalves <vitorbg92@gmail.com>
2015-12-17 14:37:37 -0200

    Trabalho Final Android SQL

--- TrabalhoAndroidSQL/app/src/main/java/compmovel/trabalhoandroidsql/ServidorActivity.java
+++ TrabalhoAndroidSQL/app/src/main/java/compmovel/trabalhoandroidsql/ServidorActivity.java
@@ -1,5 +1,8 @@
 package compmovel.trabalhoandroidsql;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +19,7 @@ public class ServidorActivity extends AppCompatActivity {
 
     private Button btnStatus;
     private TextView textViewStatus;
+    private TextView textViewInternet;
     private StatusRest statusRest = new StatusRest();
 
     @Override
@@ -25,6 +29,7 @@ public class ServidorActivity extends AppCompatActivity {
 
         btnStatus = (Button) this.findViewById(R.id.buttonStatusServidor);
         textViewStatus = (TextView) this.findViewById(R.id.textViewStatusServidor);
+        textViewInternet = (TextView) this.findViewById(R.id.textViewInternet);
 
         btnStatus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,21 +43,40 @@ public class ServidorActivity extends AppCompatActivity {
 
     private void trataCliqueStatus(View v){
         boolean status = false;
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        try {
-            status = statusRest.verificaStatusServidor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        if(isOnline()) {
+            textViewInternet.setText("INTERNET ONLINE");
 
-        System.out.println("ServidorActivity STATUS = "+status);
-        if(status){
-            textViewStatus.setText("ONLINE");
+
+
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            try {
+                status = statusRest.verificaStatusServidor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("ServidorActivity STATUS = " + status);
+            if (status) {
+                textViewStatus.setText("ONLINE");
+            } else {
+                textViewStatus.setText("OFFLINE");
+            }
         }
-        else{
+        else
+        {
             textViewStatus.setText("OFFLINE");
+            textViewInternet.setText("INTERNET OFFLINE");
         }
+    }
+
+    public boolean isOnline(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        return (networkInfo != null && networkInfo.isConnected());
 
     }
 }
